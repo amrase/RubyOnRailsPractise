30e94b7c0e2942f385caefe0336a17d599c03b1e
Amra Sezairi <amra.sezairi@hotmail.com>
2020-05-19 16:32:41 -0400

    Add edit template and update action

--- blogsapp/app/controllers/users_controller.rb
+++ blogsapp/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+    before_action :set_user,only: [:edit,:update]
+
     def new
         @user = User.new
     end    
@@ -11,9 +13,26 @@ class UsersController < ApplicationController
         else
             render 'new'
         end        
+    end 
+    
+    def edit
+      
+    end    
+
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "Your account information was successfully updated"
+            redirect_to articles_path
+        else
+            render 'edit'
+        end        
     end    
 
     private
+
+    def set_user
+        @user = User.find(params[:id])
+    end     
 
     def user_params
         params.require(:user).permit(:username,:email,:password)
--- blogsapp/config/routes.rb
+++ blogsapp/config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup',to: 'users#new'
   post 'users',to: 'users#create'
+  resources :users,expect: [:new,:create]
 end
