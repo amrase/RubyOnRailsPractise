3683059db4d9d7accf2298bcd21f8c6af44998ba
Amra Sezairi <amra.sezairi@hotmail.com>
2020-05-19 23:19:16 -0400

    users-signup

--- blogsapp/app/controllers/users_controller.rb
+++ blogsapp/app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-    before_action :set_user,only: [:edit,:update]
+    before_action :set_user,only: [:show,:edit,:update]
+    def index
+        @users = User.all
+    end
 
     def new
         @user = User.new
-    end    
+    end   
+    
+    def show
+        @articles  = @user.articles
+    end
 
     def create
         @user = User.new(user_params)
@@ -22,7 +29,7 @@ class UsersController < ApplicationController
     def update
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end        
