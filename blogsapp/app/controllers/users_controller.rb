feaa4be6617db0dfae407f4480ad6cdf865a553d
Amra Sezairi <amra.sezairi@hotmail.com>
2020-05-20 22:59:33 -0400

    current user session

--- blogsapp/app/controllers/users_controller.rb
+++ blogsapp/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user,only: [:show,:edit,:update]
     def index
-        @users = User.all
+        @users = User.paginate(page: params[:page], per_page: 5)
     end
 
     def new
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     end   
     
     def show
-        @articles  = @user.articles
+        @articles = @user.articles.paginate(page: params[:page], per_page: 5)
     end
 
     def create
